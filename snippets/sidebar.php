0dd119a0bc3ea0b8df5ee56d8e48e97f8b40cfc8
jheftmann <jheftmann@mac.com>
2015-01-25 00:16:22 -0500

    side indent

--- snippets/sidebar.php
+++ snippets/sidebar.php
@@ -1,52 +1,52 @@
-			<div class="sidebar">
-				<header>
-					<h5><a href="/">
-						Core Interaction
-						<br>
-						Spring 2015
-						</a>
-					</h5>
-					<h5>
-						Studio
-						<br>
-						PUCD 2126
-						<br>
-						Tues./Thurs. 1900&#8212;2140
-						<br>
-						Room U 306
-						<br>
-						Minnie Choi
-						<br>
-						<a href="mailto:heftmannj@newschool.edu">email@newschool.edu</a>
-					</h5>
-					<h5>
-						Lab
-						<br>
-						PUCD 2126
-						<br>
-						Mon. 1900&#8212;2140
-						<br>
-						Room U 306
-						<br>
-						Jacob Heftmann
-						<br>
-						<a href="mailto:heftmannj@newschool.edu">email@newschool.edu</a>
-					</h5>
-					<h5>
+<div class="sidebar">
+	<header>
+		<h5><a href="/">
+			Core Interaction
+			<br>
+			Spring 2015
+			</a>
+		</h5>
+		<h5>
+			Studio
+			<br>
+			PUCD 2126
+			<br>
+			Tues./Thurs. 1900&#8212;2140
+			<br>
+			Room U 306
+			<br>
+			Minnie Choi
+			<br>
+			<a href="mailto:heftmannj@newschool.edu">email@newschool.edu</a>
+		</h5>
+		<h5>
+			Lab
+			<br>
+			PUCD 2126
+			<br>
+			Mon. 1900&#8212;2140
+			<br>
+			Room U 306
+			<br>
+			Jacob Heftmann
+			<br>
+			<a href="mailto:heftmannj@newschool.edu">email@newschool.edu</a>
+		</h5>
+		<h5>
 
-					</h5>
-				</header>
-				<nav>
-					<ul>
-						<li><span class="indicator"></span><a href="#description">Description</a></li>
-						<li><span class="indicator"></span><a href="#calendar">Calendar</a></li>
-						<li><span class="indicator"></span><a href="#resources">Resources</a></li>
-						<li><span class="indicator"></span><a href="#grading">Grading</a></li>
-						<li><span class="indicator"></span><a href="#policies">Policies</a></li>
-					</ul>
-					<ul class="subnav">
-						<li><a href="#">Syllabus (PDF)</a></li>
-						<li><a href="#">Code snippets</a></li>
-					</ul>
-				</nav>
-			</div>
+		</h5>
+	</header>
+	<nav>
+		<ul>
+			<li><span class="indicator"></span><a href="#description">Description</a></li>
+			<li><span class="indicator"></span><a href="#calendar">Calendar</a></li>
+			<li><span class="indicator"></span><a href="#resources">Resources</a></li>
+			<li><span class="indicator"></span><a href="#grading">Grading</a></li>
+			<li><span class="indicator"></span><a href="#policies">Policies</a></li>
+		</ul>
+		<ul class="subnav">
+			<li><a href="#">Syllabus (PDF)</a></li>
+			<li><a href="#">Code snippets</a></li>
+		</ul>
+	</nav>
+</div>
