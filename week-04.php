a52de5b12776e14064088b619b8f3386a22bc768
jheftmann <jheftmann@mac.com>
2015-02-12 10:33:53 -0500

    add week 4

---
+++ week-04.php
@@ -0,0 +1,88 @@
+<?php include ('snippets/_header.php'); ?>
+
+
+	<div class="container-fluid">
+		<div class="inner">
+
+			<?php include ('snippets/sidebar.php'); ?>
+
+			<div class="content">
+				<header class="site-header">
+					<h6>February 16&#8212;20</h6>
+					<h1 class="z">Week 04</h1>
+				</header>
+
+				<section id="description">
+					<div class="container-fluid">
+						<div class="inner">
+							<div class="section-header">
+								<h2>Lab</h2>
+								<h3>Monday 02/16/15<br>No class</h3>
+							</div>
+							<div class="section-content">
+								<h5 class="txt-strong">Assignment: Practicing HTML</h5>
+								<p>We won’t be meeting this week because of President’s Day but .</p>
+									<ol>
+										<li>Make a free account at <a href="http://www.codecademy.com/" target="_blank">Codecademy</a>.</li>
+										<li>Complete the first section: <a href="http://www.codecademy.com/tracks/web" target="_blank">Introduction to HTML</a>.</li>
+										<li>Take a screenshot of the completed lessons and email them to me: <a href="mailto:heftmanj@newschool.edu">heftmanj@newschool.edu</a>. (New email – yay! Note that there's only one 'n' – boo!)</li>
+									</ol>
+								</p>
+								<div class="links-list">
+									<h6 class="mt-md">Reading list</h6>
+									<ul>
+										<li><a href="http://en.wikipedia.org/wiki/Tim_Berners-Lee" target="_blank">Tim Berners-Lee</a> and his <a href="http://en.wikipedia.org/wiki/World_Wide_Web" target="_blank">world wide web</a></li>
+										<li><a href="http://tanmade.com/writing/2013/10/09/designing-for-archives/" target="_blank">Designing for Archives</a></li>
+										<li><a href="http://www.wired.com/2010/02/ff_google_algorithm/all/" target="_blank">How Google’s Algorithm Rules the Web
+</a></li>
+									</ul>
+								</div>
+								<div class="links-list">
+									<h6 class="mt-md">Additional reading</h6>
+									<ul>
+										<li>If you're interested in a faster way to write markup, check out <a href="http://daringfireball.net/projects/markdown/basics" target="_blank">Markdown</a>.</li>
+									</ul>
+								</div>
+								<div class="links-list">
+									<h6 class="mt-md">Tools & resources</h6>
+									<ul>
+										<li><a href="http://www.w3schools.com/tags/">W3Schools HTML reference</a></li>
+										<li><a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element#embedded_content">Mozilla HTML reference</a></li>
+										<li><a href="http://www.sitepoint.com/web-foundations/structural-elements/">Sitepoint Structural elements</a></li>
+										<li><a href="https://github.com/jheftmann/ci-sp15/tree/master/code-samples/">Code samples</a></li>
+									</ul>
+								</div>
+								<div class="links-list">
+									<h6 class="mt-md">Practice</h6>
+									<ul>
+										<li><a href="http://www.codecademy.com/tracks/web" target="_blank">Codecademy: HTML Basics and Build Your Own Webpage modules</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<hr>
+						<div class="inner">
+							<div class="section-header">
+							<h2>Studio</h2>
+							<h3>Tuesday<br>02/17/15</h3>
+							</div>
+							<div class="section-content">
+								<p>&#8212;</p>
+							</div>
+							<div class="section-header">
+							<h2>Studio</h2>
+							<h3>Thursday<br>02/19/15</h3>
+							</div>
+							<div class="section-content">
+								<p>&#8212;</p>
+							</div>
+						</div>
+					</div>
+				</section>
+
+			</div>
+
+		</div>
+	</div>
+
+<?php include ('snippets/_footer.php'); ?>
