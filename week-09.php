498028628e4e3f3170ee36eb7ccf33479e5f9238
jheftmann <jheftmann@mac.com>
2015-03-30 11:09:52 -0700

    add week 9

---
+++ week-09.php
@@ -0,0 +1,78 @@
+<?php include ('snippets/_header.php'); ?>
+
+
+	<div class="container-fluid">
+		<div class="inner">
+
+			<?php include ('snippets/sidebar.php'); ?>
+
+			<div class="content">
+				<header class="site-header">
+					<h6>March 30&#8212;April 3</h6>
+					<h1>Week 09</h1>
+				</header>
+
+				<section id="description">
+					<div class="container-fluid">
+						<div class="inner">
+							<div class="section-header">
+								<h2>Lab</h2>
+								<h3>Monday<br>3/16/15</h3>
+							</div>
+							<div class="section-content">
+								<h5 class="txt-strong">Assignment 1&#8212;due by Monday, April 6</h5>
+								<p>Final project statement and proposal</p>
+								<ol>
+									<li><p>Create a very simple HTML page with your project statement and proposal.</p><p>Your project statement will be an explanation of the project and the connection you’re looking for in the data you collect. Your statement should sell me on your idea.
+									</p><p>Your project proposal should outline what you want to create, and how you want to create it. It might include: important milestones, deliverables, technologies required, where you’ll get your data.</p></li>
+									<li><p>Add basic styling to elegantly and clearly present the content.</p></li>
+									<li><p>Upload it to the server before next class.</p></li>
+								</ol>
+
+								<div class="links-list">
+									<h6 class="mt-md">Reading list</h6>
+									<p>No reading this week ;)</p>
+								</div>
+
+								<div class="links-list">
+									<h6 class="mt-md">Tools & resources</h6>
+									<ul>
+										<li><a href="http://www.typekit.com">Typekit</a></li>
+									</ul>
+								</div>
+
+								<div class="links-list">
+									<h6 class="mt-md">Fun links</h6>
+									<ul>
+										<li><a href="http://www.grillitype.com">Grillitype</a></li>
+									</ul>
+								</div>
+
+							</div>
+						</div>
+						<hr>
+						<div class="inner">
+							<div class="section-header">
+							<h2>Studio</h2>
+							<h3>Tuesday<br>3/17/15</h3>
+							</div>
+							<div class="section-content">
+								<!--  -->
+							</div>
+							<div class="section-header">
+							<h2>Studio</h2>
+							<h3>Thursday<br>3/19/15</h3>
+							</div>
+							<div class="section-content">
+								<!--  -->
+							</div>
+						</div>
+					</div>
+				</section>
+
+			</div>
+
+		</div>
+	</div>
+
+<?php include ('snippets/_footer.php'); ?>
